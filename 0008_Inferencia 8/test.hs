df60fe8df6a5fb2daa5386ee586a42c88479eb8e
gastonprietoutn@gmail.com <gastonprietoutn@gmail.com>
2016-05-18 20:24:19 -0400

    Mumuki Export on 2016-05-18 20:24:19 -0400

--- 0008_Inferencia 8/test.hs
+++ 0008_Inferencia 8/test.hs
@@ -1,7 +1,7 @@
 describe "Inferencia 8" $ do
   it "la expresion f1 5 4 debería tipar" $ do
-    (f1 5 4) `shouldBe` 5
+    (f1 5 $! 4) `shouldBe` 5
 
   it "la expresion f1 True 5 debería tipar" $ do
-    (f1 True 5) `shouldBe` True
+    (f1 True $! 5) `shouldBe` True
 
