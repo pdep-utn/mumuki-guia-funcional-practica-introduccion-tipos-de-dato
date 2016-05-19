79871645c923664a9f8804c7fa32c4805e06c4f0
gastonprietoutn@gmail.com <gastonprietoutn@gmail.com>
2016-05-18 21:59:35 -0400

    Mumuki Export on 2016-05-18 21:59:35 -0400

--- 0009_Inferencia 9/test.hs
+++ 0009_Inferencia 9/test.hs
@@ -1,7 +1,7 @@
 describe "Inferencia 9" $ do
   it "la expresion f 8.45 5 True debería tipar" $ do
-    (f1 8.45 5 $! True) `shouldBe` False
+    (f1 8.45 $! 5 $! True) `shouldBe` False
 
   it "la expresion f 9 False 'a' debería tipar" $ do
-    (f1 9 False $! 'a') `shouldBe` False
+    (f1 9 $! False $! 'a') `shouldBe` False
 
