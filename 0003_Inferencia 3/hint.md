7dd41fa81e2b24925a1b37e0373d72882d621eda
franco@mumuki.org <franco@mumuki.org>
2018-05-16 17:11:03 -0400

    Mumuki Export on 2018-05-16 17:11:03 -0400

--- 0003_Inferencia 3/hint.md
+++
@@ -1,4 +0,0 @@
-Pensar que las siguientes expresiones deberían tipar:
-
-* `f1 3 6`
-* `f1 4.5 0`
--- 0003_Inferencia 3/test.hs
+++
@@ -1,7 +0,0 @@
-describe "Inferencia 3" $ do
-  it "la expresion f1 3 6 debería tipar" $ do
-    f1 3 6 `shouldBe` 18
-
-  it "la expresion f1 4.5 0 debería tipar" $ do
-    f1 4.5 0 `shouldBe` 0
-
