eaf0e8993a84952a2954003764e410b54a3d9c49
franco@mumuki.org <franco@mumuki.org>
2018-05-17 15:10:00 -0400

    Mumuki Export on 2018-05-17 15:10:00 -0400

--- 0009_Inferencia 10/hint.md
+++
@@ -1,6 +0,0 @@
-Pensar que las siguientes expresiones deberían tipar:
-
-* `f 8.45 5 True`
-* `f 9 False 'a'`
-
-Recordá que no todos los números pueden compararse por igual
--- 0009_Inferencia 10/test.hs
+++
@@ -1,7 +0,0 @@
-describe "Inferencia 9" $ do
-  it "la expresion f 8.45 5 True debería tipar" $ do
-    (f1 8.45 5 True) `shouldBe` False
-
-  it "la expresion f 9 False 'a' debería tipar" $ do
-    (f1 9 False 'a') `shouldBe` False
-
