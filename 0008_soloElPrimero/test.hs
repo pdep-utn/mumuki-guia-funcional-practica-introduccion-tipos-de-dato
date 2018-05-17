300e939583afe7fd16438fb1685f0c57459d7399
franco@mumuki.org <franco@mumuki.org>
2018-05-17 13:49:21 -0400

    Mumuki Export on 2018-05-17 13:49:21 -0400

--- 0008_soloElPrimero/test.hs
+++ 0008_soloElPrimero/test.hs
@@ -1,7 +1,7 @@
 describe "Inferencia 8" $ do
-  it "la expresion f1 5 4 debería tipar" $ do
-    (f1 5 $! 4) `shouldBe` 5
+  it "la expresion soloElPrimero 5 4 debería tipar" $ do
+    (soloElPrimero 5 $! 4) `shouldBe` 5
 
-  it "la expresion f1 True 5 debería tipar" $ do
-    (f1 True $! 5) `shouldBe` True
+  it "la expresion soloElPrimero True 5 debería tipar" $ do
+    (soloElPrimero True $! 5) `shouldBe` True
 
