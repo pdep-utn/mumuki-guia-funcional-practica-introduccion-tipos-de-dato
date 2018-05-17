9ab871689bcc67958160717824f6060735098a86
franco@mumuki.org <franco@mumuki.org>
2018-05-17 16:21:54 -0400

    Mumuki Export on 2018-05-17 16:21:54 -0400

--- 0010_Inferencia 11/extra.hs
+++
@@ -1,4 +0,0 @@
-f1 x = g x 4
-
-g :: Ord a => a -> a -> a
-g = max
--- 0010_Inferencia 11/test.hs
+++
@@ -1,7 +0,0 @@
-describe "Inferencia 10" $ do
-  it "la expresion f 7 debería tipar" $ do
-    (f1 7) `shouldBe` 7
-
-  it "la expresion f 9 debería tipar" $ do
-    (f1 9) `shouldBe` 9
-
